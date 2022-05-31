9734c80b5ce89e5316d6f3199790cca632f58f2e
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2022-05-31 22:30:46 +0530

    Authentication working perfectly.

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
