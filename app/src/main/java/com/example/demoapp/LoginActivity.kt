60cf544d4a35583a4d0e09e72316b8b40cc83b1e
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2022-05-31 21:58:13 +0530

    Sign up and Login logic added.

--- app/src/main/java/com/example/demoapp/LoginActivity.kt
+++ app/src/main/java/com/example/demoapp/LoginActivity.kt
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.net.PasswordAuthentication
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        auth = Firebase.auth
 
         val submit = findViewById<Button>(R.id.button2)
         val etEmail = findViewById<EditText>(R.id.editTextTextEmailAddress)
@@ -18,7 +28,7 @@ class LoginActivity : AppCompatActivity() {
         submit.setOnClickListener {
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
-            login(email, password)
+            signIn(email, password)
         }
 
 
@@ -31,8 +41,21 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    fun login(email: String, password: String){
-
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this, BottomNavActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(baseContext, "Authentication Successful.", Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        // [END sign_in_with_email]
     }
 
 }
--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.demoapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
+        val user = auth.currentUser
+        if(user!=null){
+            val intent = Intent(this, BottomNavActivity::class.java)
+            startActivity(intent)
+        }
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -18,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         submit.setOnClickListener {
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
-            signUp(email, password)
+            createAccount(email, password)
         }
 
         val button = findViewById<Button>(R.id.button)
@@ -29,7 +46,20 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun signUp(email: String, password: String){
-
+    private fun createAccount(email: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, BottomNavActivity::class.java)
+                    startActivity(intent)
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(baseContext, "Authentication Successful.", Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
+
 }
