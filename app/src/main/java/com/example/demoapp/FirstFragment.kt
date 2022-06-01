91949faa6f4639f28f7914605fc555abb360938c
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2022-06-01 12:43:07 +0530

    Graph added.'

--- app/src/main/java/com/example/demoapp/FirstFragment.kt
+++ app/src/main/java/com/example/demoapp/FirstFragment.kt
@@ -1,19 +1,35 @@
 package com.example.demoapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import kotlin.random.Random
 
 
 class FirstFragment : Fragment() {
 
+    private lateinit var months: Spinner
+    private lateinit var graph: GraphView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        months = view.findViewById(R.id.months)
+        graph = view.findViewById(R.id.graph)
+
+
+        setData()
     }
 
     override fun onCreateView(
@@ -22,6 +38,22 @@ class FirstFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+    fun setData(){
+
+        var dataSet = mutableListOf<DataPoint>(DataPoint(1.0, 3.5))
+        for(i in 2..30){
+            dataSet.add(DataPoint(i.toDouble(), Random.nextDouble()))
+        }
+
+
+        val series = LineGraphSeries(
+            dataSet.toTypedArray()
+        )
+
+        graph.addSeries(series)
     }
 
 }
